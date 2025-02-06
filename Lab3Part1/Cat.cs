981eea6cdb67d979d14ee644eab506af553ae63c
jtumbokon1 <jirchgot@gmail.com>
2025-02-06 11:18:37 -0700

    Added dog and cat

---
+++ Lab3Part1/Cat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3Part1
+{
+    public class Cat : Animal
+    {
+        // constructor
+        public Cat(string name, string colour, int age) : 
+            base(name, colour, age)
+        {
+        }
+
+        // public methods
+        public override void Eat()
+        {
+            Console.WriteLine("Cats eat mice."); ;
+        }
+    } // class
+} // namespace
---
+++ Lab3Part1/Dog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3Part1
+{
+    public class Dog : Animal
+    {
+
+        // constructor
+        public Dog(string name, string colour, int age) : 
+            base(name, colour, age)
+        {
+        }
+
+        // public methods
+        public override void Eat()
+        {
+            Console.WriteLine("Dogs eat meat.");
+        }
+    } // class
+} // namespace
